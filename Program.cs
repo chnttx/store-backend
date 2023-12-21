38e125cdccc06dbd105da3dcced614daeeabf47a
harrynguyen864 <n11t2h79ette@gmail.com>
2023-12-21 16:59:14 +0700

    added logging support

--- Program.cs
+++ Program.cs
@@ -26,6 +26,11 @@ builder.Services.AddMvc(options =>
 {
     options.Filters.Add(typeof(SecretKeyAttribute));
 });
+builder.Services.AddLogging(logging =>
+{
+    logging.AddConsole();
+    logging.AddDebug();
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
