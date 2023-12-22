2f1838a5758889826fe20772e3636672cb8d268a
harrynguyen864 <n11t2h79ette@gmail.com>
2023-12-22 11:19:53 +0700

    moved validation inside/added updateVIP

--- Services/HourlyBackgroundService.cs
+++ Services/HourlyBackgroundService.cs
@@ -1,12 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication2.Data;
+using WebApplication2.Models;
+
 namespace WebApplication2.Services;
 
 public class HourlyBackgroundService : BackgroundService
 {
+    private DataContext _context;
     private ILogger<HourlyBackgroundService> _logger;
 
-    public HourlyBackgroundService(ILogger<HourlyBackgroundService> logger)
+    public HourlyBackgroundService(DataContext context, ILogger<HourlyBackgroundService> logger)
     {
         _logger = logger;
+        _context = context;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -17,7 +23,27 @@ public class HourlyBackgroundService : BackgroundService
             if (currentTime is { Minute: 0, Second: 0 })
             {
                 _logger.LogInformation("Updating VIP customers");
+                UpdateVipCustomers();
             }
+
+            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
         }
+    }
+
+    private void UpdateVipCustomers()
+    {
+        var vipCustomers = ( 
+            from o in _context.Orders
+            join oi in _context.OrderItems on o.OrderId equals oi.OrderId
+            group oi by o.CustomerId into g
+                where g.Select(oi => oi.ItemId).Distinct().Count() > 10 || 
+                      g.Sum(oi => oi.ItemPrice * oi.Quantity) > 1e6
+                      select g.Key).ToList();
+        foreach (var customer in vipCustomers.Select(customerId => _context.Customers.Find(customerId)).OfType<Customer>())
+        {
+            customer.IsVIP = 1;
+        }
+
+        _context.SaveChanges();
     }
 }
