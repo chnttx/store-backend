1c35e14bd62c44815834b6532b46fbb5e82cc765
harrynguyen864 <n11t2h79ette@gmail.com>
2023-12-22 11:22:46 +0700

    patched things up

--- obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca1b2e42e9fd530b525caf422375ee99f9dc7a5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52da2c239d41fcafaea2bee13e145c806551deb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
