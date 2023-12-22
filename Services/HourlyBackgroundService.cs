66f9b59e1708adf72cb3a5057989450e926435e4
harrynguyen864 <n11t2h79ette@gmail.com>
2023-12-22 11:38:57 +0700

    fixed singleton context

--- Services/HourlyBackgroundService.cs
+++ Services/HourlyBackgroundService.cs
@@ -6,44 +6,43 @@ namespace WebApplication2.Services;
 
 public class HourlyBackgroundService : BackgroundService
 {
-    private DataContext _context;
-    private ILogger<HourlyBackgroundService> _logger;
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<HourlyBackgroundService> _logger;
 
-    public HourlyBackgroundService(DataContext context, ILogger<HourlyBackgroundService> logger)
+    public HourlyBackgroundService(IServiceProvider serviceProvider, ILogger<HourlyBackgroundService> logger)
     {
         _logger = logger;
-        _context = context;
+        _serviceProvider = serviceProvider;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
         {
-            var currentTime = DateTime.UtcNow;
-            if (currentTime is { Minute: 0, Second: 0 })
+            using (var scope = _serviceProvider.CreateScope())
             {
+                var context = scope.ServiceProvider.GetRequiredService<DataContext>();
                 _logger.LogInformation("Updating VIP customers");
-                UpdateVipCustomers();
+                UpdateVipCustomers(context);
             }
-
             await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
         }
     }
 
-    private void UpdateVipCustomers()
+    private void UpdateVipCustomers(DataContext context)
     {
         var vipCustomers = ( 
-            from o in _context.Orders
-            join oi in _context.OrderItems on o.OrderId equals oi.OrderId
+            from o in context.Orders
+            join oi in context.OrderItems on o.OrderId equals oi.OrderId
             group oi by o.CustomerId into g
                 where g.Select(oi => oi.ItemId).Distinct().Count() > 10 || 
                       g.Sum(oi => oi.ItemPrice * oi.Quantity) > 1e6
                       select g.Key).ToList();
-        foreach (var customer in vipCustomers.Select(customerId => _context.Customers.Find(customerId)).OfType<Customer>())
+        foreach (var customer in vipCustomers.Select(customerId => context.Customers.Find(customerId)).OfType<Customer>())
         {
             customer.IsVIP = 1;
         }
 
-        _context.SaveChanges();
+        context.SaveChanges();
     }
 }
